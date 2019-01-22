de3e2ed6f89fb8372c53555d9b7857d1686f2365
murakamiii <9khc727ru7@gmail.com>
2019-01-22 23:02:52 +0900

    Todo新規作成ページ

---
+++ lib/newTodoPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewTodoPage extends StatefulWidget {
+  NewTodoPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _NewTodoPageState createState() => _NewTodoPageState();
+}
+
+class _NewTodoPageState extends State<NewTodoPage> {
+  @override
+  Widget build(BuildContext context) {}
+}
