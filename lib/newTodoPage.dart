1213aa7f9b500b85484fb8e9976aadf4710909d4
murakamiii <9khc727ru7@gmail.com>
2019-01-30 03:26:53 +0900

    decorate text and textField

--- lib/newTodoPage.dart
+++ lib/newTodoPage.dart
@@ -13,12 +13,26 @@ class _NewTodoPageState extends State<NewTodoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("新規作成"),
-      ),
-      body: Center(
-        child: TextField(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("新規作成"),
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(16, 160, 16, 160),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Text(
+              "やること：",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "やること",
+                  contentPadding: EdgeInsets.fromLTRB(8, 12, 8, 0)),
+              style: TextStyle(fontSize: 20, color: Colors.black87),
+            ),
+          ]),
+        ));
   }
 }
